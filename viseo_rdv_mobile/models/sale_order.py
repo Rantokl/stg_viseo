920dc69aa5382b3317c4c499ebb2dc9697e2b7c3
Ariscreamo <henintsoaariel@yahoo.com>
2024-06-05 09:14:09 +0300

    [UPDATE]

---
+++ viseo_rdv_mobile/models/sale_order.py
@@ -0,0 +1,30 @@
+# -*- coding:utf-8 -*-
+
+from odoo import fields, api, models
+from odoo.exceptions import ValidationError
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    bypass_vin_sale = fields.Boolean(default=False,copy=False,track_visibility=True)
+
+    def write(self, vals):
+        res = super(SaleOrder,self).write(vals)
+        if 'state' in vals:
+            if vals['state'] == 'sale':
+                veh_qty = 0
+                # sale_order = self.env['sale.order'].search([('name', '=', self.invoice_origin)])
+                for sales in filter(lambda x: x.product_id.model_id, self.order_line): veh_qty += sales.product_uom_qty
+                if veh_qty >= 1:
+                    if (self.vehicle_ids is None) and not self.bypass_vin_sale:
+                        raise ValidationError(('Ajouter le(s) VIN de véhicule(s) dans la vente'))
+                    else:
+                        if veh_qty != len(self.vehicle_ids) and not self.bypass_vin_sale:
+                            raise ValidationError(('Ajuster le nombre de véhicule suivant la commande dans le vente'))
+        return res
+
+    def action_bypass_vin(self):
+        for rec in self:
+            rec.update({
+                'bypass_vin_sale': True
+            })
